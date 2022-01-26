1bc21b505a41759c99e96ea9d8d4884e069b4fbe
joshhavi <70553454+joshhavi@users.noreply.github.com>
2022-01-26 14:45:21 -0700

    Update TableCreation.sql
    Adding double quotes to table names, change from INT to SERIAL for ID's

--- TableCreation.sql
+++ TableCreation.sql
@@ -1,4 +1,4 @@
-CREATE TABLE Group
+CREATE TABLE "Group"
 (
   GroupInviteCode VARCHAR(12) NOT NULL,
   GroupName VARCHAR(255) NOT NULL,
@@ -6,25 +6,25 @@ CREATE TABLE Group
   GroupColor VARCHAR(255) NOT NULL,
   GroupImage VARCHAR(255),
   GroupAdmin INT NOT NULL,
-  GroupID INT NOT NULL,
+  GroupID SERIAL NOT NULL,
   PRIMARY KEY (GroupID),
   UNIQUE (GroupInviteCode)
 );
 
-CREATE TABLE User
+CREATE TABLE "User"
 (
   FirstName VARCHAR(255) NOT NULL,
   LastName VARCHAR(255) NOT NULL,
   EmailAddress VARCHAR(255) NOT NULL,
   IsAdmin INT NOT NULL,
   UserPassword VARCHAR(255) NOT NULL,
-  UserID INT NOT NULL,
+  UserID SERIAL NOT NULL,
   DefaultAvailableTimes  NOT NULL,
   PRIMARY KEY (UserID),
   UNIQUE (EmailAddress)
 );
 
-CREATE TABLE GroupHasUser
+CREATE TABLE "GroupHasUser"
 (
   UserNickname INT NOT NULL,
   AvailableTimes INT NOT NULL,
@@ -35,7 +35,7 @@ CREATE TABLE GroupHasUser
   FOREIGN KEY (GroupID) REFERENCES Group(GroupID)
 );
 
-CREATE TABLE User_PhoneNumber
+CREATE TABLE "User_PhoneNumber"
 (
   PhoneNumber INT NOT NULL,
   UserID INT NOT NULL,
@@ -43,19 +43,19 @@ CREATE TABLE User_PhoneNumber
   FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
 
-CREATE TABLE Event
+CREATE TABLE "Event"
 (
   EventName VARCHAR(255) NOT NULL,
   EventDesc VARCHAR(255),
   EventDuration FLOAT NOT NULL,
   EventStartTime DATE NOT NULL,
-  EventID INT NOT NULL,
+  EventID SERIAL NOT NULL,
   GroupID INT NOT NULL,
   PRIMARY KEY (EventID),
   FOREIGN KEY (GroupID) REFERENCES Group(GroupID)
 );
 
-CREATE TABLE UserAttendsEvent
+CREATE TABLE "UserAttendsEvent"
 (
   EventID INT NOT NULL,
   UserID INT NOT NULL,
