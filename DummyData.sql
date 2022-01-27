99bde5cca7aa56c06e10d9a41c706550b001313a
jeffcrow5 <70713137+jeffcrow5@users.noreply.github.com>
2022-01-27 09:24:27 -0700

    Update DummyData.sql
    Changed SQL to (more) correct data

--- DummyData.sql
+++ DummyData.sql
@@ -1,15 +1,18 @@
-INSERT INTO "PATRON" (patronName, patronPassword, patronDateCreated, isAdmin, patronEmail)
-VALUES ('Kevin', 'testing', '1/30/2021', '1', 'Kevinsucks@gmail.com');
+INSERT INTO "User" (FirstName, LastName, EmailAddress, IsAdmin, UserPassword, DefaultAvailableTimes)
+VALUES ('Rick', 'Astley', 'nevergonnagive@you.up', '1', 'hashedPassword', '[11200, 41300, 51400]');
 
 
-INSERT INTO "DNDCLASS" (deity, hitDie, dndClassName, subClass, savingThrow, primaryAttribute, equipmentProf, dndClassDescription)
-VALUES ('God', '10', 'Paladin', 'Redemption', 'Con, Wis', 'Charisma', 'Heavy Armor, Smithing Tools', 'Awesome');
+INSERT INTO "Group" (GroupInviteCode, GroupName, GroupDesc, GroupColor, GroupImage, GroupAdmin)
+VALUES ('123456781234', 'Rick Rollers', 'Got em', '#FF0000', 'test.png', '1');
 
-INSERT INTO "RACE" (history, size, speed, raceName)
-VALUES ('They are old', 'Medium', '30', 'Elf');
+INSERT INTO "GroupHasUser" (UserID, GroupID, UserNickname, AvailableTimes)
+VALUES ('1', '1', 'Rick Roll', '[41300, 51400]');
 
-INSERT INTO "PHYSICALITY" (height, hairColor, weight, age, skinColor)
-VALUES ('5.7', 'Pink', '406', '1', 'Purple');
+INSERT INTO "Event" (EventName, EventDesc, EventDuration, EventStartTime, GroupID)
+VALUES ('Rick Roll Party', 'We're going to make sure everyone knows we aren\'t going to give them up or let them down', '2.0', '2022-01-30 13:00:00', '1');
 
-INSERT INTO "CHARACTER" (charName, charLevel, feat, strength, intelligence, charisma, constitution, wisdom, dexterity, hitPoints, flaw, bond, ideal, background, patronID, dndClassID, raceID, physicalityID)
-VALUES ('Kevin Cha', '22', 'diety', '18', '17', '16', '15', '14', '13', '420', 'He is a big dummy at everything', 'He likes glue - glue eater', 'Yes he is ideal', 'He likes other idiots', '1', '1', '1', '1');
+INSERT INTO "UserAttendsEvent" (EventID, UserID)
+VALUES ('1', '1');
+
+INSERT INTO "User_PhoneNumber" (UserID, PhoneNumber)
+VALUES ('1', '8074251655');
