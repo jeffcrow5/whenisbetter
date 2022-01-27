ea93f4679d4c8b6425013c1fff34de82da2a9990
jeffcrow5 <70713137+jeffcrow5@users.noreply.github.com>
2022-01-27 12:48:12 -0700

    SQL under construction

--- DummyData.sql
+++ DummyData.sql
@@ -1,18 +1,21 @@
 INSERT INTO "User" (FirstName, LastName, EmailAddress, IsAdmin, UserPassword, DefaultAvailableTimes)
-VALUES ('Rick', 'Astley', 'nevergonnagive@you.up', '1', 'hashedPassword', '[11200, 41300, 51400]');
-
+VALUES ('Rick', 'Astley', 'nevergonnagive@you.up', '1', 'hashedPassword', '{"11200","41300","51400"}');
 
 INSERT INTO "Group" (GroupInviteCode, GroupName, GroupDesc, GroupColor, GroupImage, GroupAdmin)
 VALUES ('123456781234', 'Rick Rollers', 'Got em', '#FF0000', 'test.png', '1');
 
 INSERT INTO "GroupHasUser" (UserID, GroupID, UserNickname, AvailableTimes)
-VALUES ('1', '1', 'Rick Roll', '[41300, 51400]');
+VALUES ('1', '1', 'Rick Roll', '{"41300","51400"}');
+
+
+INSERT INTO "GroupHasUserTimes" (UserID, PhoneNumber)
+VALUES ('1', '8074251655');
 
 INSERT INTO "Event" (EventName, EventDesc, EventDuration, EventStartTime, GroupID)
-VALUES ('Rick Roll Party', 'We're going to make sure everyone knows we aren\'t going to give them up or let them down', '2.0', '2022-01-30 13:00:00', '1');
+VALUES ('Rick Roll Party', E'We\'re going to make sure everyone knows we aren\'t going to give them up or let them down', '2.0', '2022-01-30 13:00:00-07', '1');
 
 INSERT INTO "UserAttendsEvent" (EventID, UserID)
 VALUES ('1', '1');
 
-INSERT INTO "User_PhoneNumber" (UserID, PhoneNumber)
+INSERT INTO "UserPhoneNumber" (UserID, PhoneNumber)
 VALUES ('1', '8074251655');
