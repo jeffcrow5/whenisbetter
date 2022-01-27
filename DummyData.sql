78e3e9fc4cfd49fb9fa0f5ddf3e339009c127516
joshhavi <70553454+joshhavi@users.noreply.github.com>
2022-01-27 12:55:01 -0700

    Update DummyData.sql
    Added SQL for updated database

--- DummyData.sql
+++ DummyData.sql
@@ -1,15 +1,17 @@
-INSERT INTO "User" (FirstName, LastName, EmailAddress, IsAdmin, UserPassword, DefaultAvailableTimes)
-VALUES ('Rick', 'Astley', 'nevergonnagive@you.up', '1', 'hashedPassword', '{"11200","41300","51400"}');
+INSERT INTO "User" (FirstName, LastName, EmailAddress, IsAdmin, UserPassword)
+VALUES ('Rick', 'Astley', 'nevergonnagive@you.up', '1', 'hashedPassword');
+
+INSERT INTO "UserDefaultTimes" (WeekDay, Hour, UserID)
+VALUES ('Sunday', '13', '1');
 
 INSERT INTO "Group" (GroupInviteCode, GroupName, GroupDesc, GroupColor, GroupImage, GroupAdmin)
 VALUES ('123456781234', 'Rick Rollers', 'Got em', '#FF0000', 'test.png', '1');
 
-INSERT INTO "GroupHasUser" (UserID, GroupID, UserNickname, AvailableTimes)
-VALUES ('1', '1', 'Rick Roll', '{"41300","51400"}');
+INSERT INTO "GroupHasUser" (UserID, GroupID, UserNickname)
+VALUES ('1', '1', 'Rick Roll');
 
-
-INSERT INTO "GroupHasUserTimes" (UserID, PhoneNumber)
-VALUES ('1', '8074251655');
+INSERT INTO "GroupHasUserTimes" (WeekDay, Hour, GroupHasUserID)
+VALUES ('Sunday', '13', '1');
 
 INSERT INTO "Event" (EventName, EventDesc, EventDuration, EventStartTime, GroupID)
 VALUES ('Rick Roll Party', E'We\'re going to make sure everyone knows we aren\'t going to give them up or let them down', '2.0', '2022-01-30 13:00:00-07', '1');
