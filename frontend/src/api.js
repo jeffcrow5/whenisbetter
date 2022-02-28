00602aa4116f56c2f1dda4c97ab47e68351b2746
Jeffrey Crowley <jcrowley2465@gmail.com>
2022-02-28 14:45:37 -0700

    Changed API URL

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import { authHeader, getJwtToken, getUserIdFromToken } from "./auth";
 
-const API_URL = "http://localhost:8000";
+const API_URL = "http://54.205.253.120/:8000";
 
 class Api {
   getArticles() {
