463847a759df435f1ce9f415d1c94fa2db4753cd
Jeffrey Crowley <jcrowley2465@gmail.com>
2022-03-16 13:43:20 -0600

    Updating admin panel

--- frontend/src/pages/Admin.vue
+++ frontend/src/pages/Admin.vue
@@ -8,20 +8,41 @@
     </b-form-radio-group>
     <hr>
 
+    <!-- Group View -->
     <div class="view" v-if="activeTab == 'Group View'">
       <div class="left">
-        Group left side
+        Group to Modify
+        <b-form-select v-model="groupSelected" :options="groups.map(group => {
+          return {text: group.groupname, value: group.groupid}})" />     
       </div>
+      <!-- Group View -->
       <div class="right">
-        Group right side
+        <!-- Members -->
+        <div class="list">
+          <p>Members</p>
+          <div v-for="(member, index) in groupMembers" :key="index">{{`${member.firstname} ${member.lastname}`}}<b-button @click="deleteMember(member)" variant="danger" class="ml-auto">Delete</b-button></div>
+        </div>
+
+        <!-- Events -->
+        <div class="list">
+          <p>Events</p>
+          <div v-for="(event, index) in groupEvents" :key="index">{{event.eventname}}<b-button @click="deleteEvent(event)" variant="danger">Delete</b-button></div>
+        </div>
       </div>
     </div>
+
+    <!-- User View -->
     <div class="view" v-else>
       <div class="left">
-        User left side
+        User to Modify
+        <b-form-select v-model="userSelected" :options="users.map((user) => {return {text: `${user.firstname} ${user.lastname}`, value: user.userid}})" />     
       </div>
       <div class="right">
-        User right side
+        <b-form-checkbox
+          v-model="userSelected.isAdmin"
+        >
+          Administrator
+        </b-form-checkbox>
       </div>
     </div>
   </div>
@@ -34,11 +55,32 @@ export default {
   name: "Admin",
   data () {
     return {
-      activeTab: 'Group View'
+      activeTab: 'Group View',
+      groupSelected: {},
+      groups: [{groupname: 'group 1', groupid: 1}, {groupname: 'group 2', groupid: 2}, {groupname: 'Group 3', groupid: 3}],
+      groupMembers: [],
+      groupEvents: [],
+
+      userSelected: {},
+      users: [{userid: 1, firstname: 'Jeffrey', lastname: 'Crowley'}, {userid: 2, firstname: 'Trevor', lastname: 'McClellan'}, {userid: 3, firstname: 'Josh', lastname: 'Haviland'}, {userid: 4, firstname: 'Derek', lastname: 'Hansen'}]
     }
   },
   created() {
     Api
+    // TODO: Api.getAllGroups()
+  },
+  watch: {
+    groupSelected(newValue, oldValue) {
+      if (newValue != oldValue) {
+        this.groupMembers = [{userid: 1, firstname: 'Jeffrey', lastname: 'Crowley'}, {userid: 2, firstname: 'Trevor', lastname: 'McClellan'}, {userid: 3, firstname: 'Josh', lastname: 'Haviland'}]
+        // TODO: Api set groupMembers and groupEvents
+      }
+    },
+    userSelected(newValue, oldValue) {
+      if (newValue != oldValue) {
+        this
+      }
+    }
   }
 };
 </script>
@@ -63,5 +105,11 @@ export default {
 }
 .right {
   grid-area: right;
+}
+
+.list {
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
 }
 </style>
