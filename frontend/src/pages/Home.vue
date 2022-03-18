5a29f34c34b83df68a2cab38dd66a279d91e54f8
Trevor McClellan <tsmcclel@byu.edu>
2022-03-18 13:19:58 -0600

    Add group times when joining a group

--- frontend/src/pages/Home.vue
+++ frontend/src/pages/Home.vue
@@ -35,6 +35,111 @@
       </b-modal>
 
       <b-modal
+      size="xl"
+      :title="newGroupTitle"
+      id="group-times-modal"
+      @ok="setGroupTimes">
+        <b-form
+          @submit.prevent="
+            setGroupTimes();
+            $bvModal.hide('group-times-modal')
+          "
+        >
+          <b-form-group
+            label="Monday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="monday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Tuesday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="tuesday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Wednesday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="wednesday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Thursday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="thursday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Friday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="friday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Saturday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="saturday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+          <b-form-group
+            label="Sunday Availability"
+            v-slot="{ ariaDescribedby }"
+          >
+            <b-form-checkbox-group
+              v-model="sunday"
+              :options="options"
+              :aria-describedby="ariaDescribedby"
+              name="buttons-1"
+              buttons
+              button-variant="info"
+            ></b-form-checkbox-group>
+          </b-form-group>
+        </b-form>
+      </b-modal>
+
+      <b-modal
       size="md"
       title="Enter Group Info"
       id="create-group-modal"
@@ -221,13 +326,51 @@ export default {
         eventstartdate: ''
       },
       userAttendingEvent: false,
+      newGroup: {},
+      newGroupTitle: '',
+      user: {},
+      monday: [],
+      tuesday: [],
+      wednesday: [],
+      thursday: [],
+      friday: [],
+      saturday: [],
+      sunday: [],
+      options: [
+          { text: '6am', value: '6' },
+          { text: '7am', value: '7' },
+          { text: '8am', value: '8' },
+          { text: '9am', value: '9' },
+          { text: '10am', value: '10' },
+          { text: '11am', value: '11' },
+          { text: '12pm', value: '12' },
+          { text: '1pm', value: '13' },
+          { text: '2pm', value: '14' },
+          { text: '3pm', value: '15' },
+          { text: '4pm', value: '16' },
+          { text: '5pm', value: '17' },
+          { text: '6pm', value: '18' },
+          { text: '7pm', value: '19' },
+          { text: '8pm', value: '20' },
+        ],
     };
   },
   created() {
     Api.getUserGroups().then(response => {
-        this.groups = response.data
-        this.selectedGroup = this.groups[0]
-      })
+      this.groups = response.data
+      this.selectedGroup = this.groups[0]
+    })
+    Api.getCurrentUser().then(response => {
+      this.user = response.data[0]
+      let defaulttimes = response.data[0].defaulttimes
+      this.monday = defaulttimes.monday || []
+      this.tuesday = defaulttimes.tuesday || []
+      this.wednesday = defaulttimes.wednesday || []
+      this.thursday = defaulttimes.thursday || []
+      this.friday = defaulttimes.friday || []
+      this.saturday = defaulttimes.saturday || []
+      this.sunday = defaulttimes.sunday || []
+    })
   },
   watch: {
     selectedGroup(newValue, oldValue) {
@@ -290,10 +433,35 @@ export default {
     joinGroup() {
       Api.getGroupFromInviteCode(this.groupInviteCode).then(response => {
         let group = response.data[0]
-        let userid = getUserIdFromToken(getJwtToken())
-        Api.addUserToGroup(userid, group.groupid).then(() => {
-          this.getUserGroups()
-        })
+        this.setGroupDefaultTimes(group)
+      })
+    },
+    setGroupDefaultTimes(group) {
+      this.newGroup = group
+      this.newGroupTitle = `Welcome to ${group.groupname}!`
+      this.$bvModal.show('group-times-modal')
+    },
+    setGroupTimes() {
+      let grouptimes = {
+        monday: this.monday,
+        tuesday: this.tuesday,
+        wednesday: this.wednesday,
+        thursday: this.thursday,
+        friday: this.friday,
+        saturday: this.saturday,
+        sunday: this.sunday
+      }
+      Api.addUserToGroup(this.user.userid, this.newGroup.groupid, grouptimes).then(() => {
+        this.getUserGroups()
+        this.newGroupTitle = ''
+        this.newGroup = {}
+        this.monday = []
+        this.tuesday = []
+        this.wednesday = []
+        this.thursday = []
+        this.friday = []
+        this.saturday = []
+        this.sunday = []
       })
     },
     leaveGroup() {
